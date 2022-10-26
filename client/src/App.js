ef9739f4ead17d48d20470063a2290a045b39769
Jacob Yaw <yawjacob@yahoo.com>
2022-10-26 15:53:19 -0500

    creating dummy shop component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Home from "./Home";
+import Shop from "./Shop";
  
 function App() {
   return (
     <div>
       <h1>hello this is the app</h1>
       <Home/>
+      <Shop/>
     </div>
   );
 }
---
+++ client/src/Shop.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Shop() {
+
+    console.log('hello')
+
+    return (
+        <h1>the shop page</h1>
+    )
+}
+
+export default Shop
