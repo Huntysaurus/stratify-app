f9690404eebd4e72c23fb97d2450febfb38d2b9f
Jacob Yaw <yawjacob@yahoo.com>
2022-10-26 21:07:46 -0500

    adding products to the dom

---
+++ client/src/Product.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Product({ product }) {
+
+    return (
+        <div>
+            <h3>{product.name}</h3>
+            <img src={product.image} alt={product.name}/>
+            <p>{product.category}</p>
+            <p>{product.price}</p>
+            <p>{product.description}</p>
+        </div>
+    )
+}
+
+export default Product
--- client/src/Shop.js
+++ client/src/Shop.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Product from "./Product";
 
 function Shop() {
+    const [products, setProducts] = useState([])
 
-    console.log('hello')
+    console.log(products)
+
+    useEffect(()=> {
+        fetch('/products')
+        .then((r)=> {
+            if (r.ok) {
+                r.json().then((products) => setProducts(products))
+            } else {
+                r.json().then(err => console.log(err.errors))
+            }
+        }, [products.id])
+    })
 
     return (
-        <h1>the shop page</h1>
+        <div>
+            <h1>the shop page</h1>
+            <div>
+                {products.map(product => {
+                    return <Product key={product.id} product={product} />
+                })}
+            </div>
+        </div>
     )
 }
 
