d2f485083572630f84980cf834b1e6fca3d2e18e
Jacob Yaw <yawjacob@yahoo.com>
2022-11-03 20:11:22 -0500

    made dummy booleon forms for changing password and username on the profile

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -3,6 +3,8 @@ import Review from "./Review";
 
 function Profile({ user }) {
     const [reviews, setReviews] = useState([])
+    const [userForm, setUserForm] = useState(null)
+    const [passwordForm, setPasswordForm] = useState(null)
 
     useEffect(()=>{
         fetch(`/user/${user.id}/reviews`)
@@ -10,20 +12,50 @@ function Profile({ user }) {
         .then(reviews => setReviews(reviews))
     }, [])
 
+    function onPasswordChangeClick(){
+        setPasswordForm(true)
+    }
+
     return (
         <div>
             <h1>Profile</h1>
             <div>
                 <h2>{user.name}</h2>
                 <p>username: {user.username}</p>
+            <div>
+                {
+                    passwordForm ?
+                    <form>
+                        <button onClick={()=>setPasswordForm(null)}>cancel</button>
+                        <p>the form</p>
+                    </form>
+                    :
+                    <button onClick={onPasswordChangeClick}>change password</button>
+                }
+            </div>
+            </div>
+            <div>
+                {
+                    userForm ?
+                    <form>
+                        <button onClick={()=>setUserForm(null)}>cancel</button>
+                        <p>the form</p>
+                    </form>
+                    :
+                    <button onClick={()=>setUserForm(true)}>change username</button>
+                }
+            </div>
+            <div>
                 <div>
                     <h2>Your orders</h2>
+                    <p>future order invoices go here</p>
                 </div>
                 <div>
                     <h2>Your reviews</h2>
                     {reviews.map((review) => <Review review={review}/>)}
                 </div>
             </div>
+
         </div>
     )
 }
--- client/src/Review.js
+++ client/src/Review.js
@@ -6,7 +6,7 @@ function Review({ review }) {
             <h2>{review.product.name}</h2>
             <h3>category: {review.product.category}</h3>
             <h3>price: ${review.product.price}</h3>
-            <img src={review.product.image} />
+            <img src={review.product.image} alt={review.product.name}/>
             <h3>{review.description}</h3>
         </div>
     )
