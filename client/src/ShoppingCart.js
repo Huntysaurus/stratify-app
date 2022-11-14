4915d6daa4a1170da51d2b88669529bdae3e974b
Jacob Yaw <yawjacob@yahoo.com>
2022-11-13 18:53:24 -0600

    created functionality to configure the total. The remove feature is not working correctly

--- client/src/ShoppingCart.js
+++ client/src/ShoppingCart.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import CartItem from "./CartItem";
 
 function ShoppingCart({ cartProducts, onRemoveClick }) {
     const navigate = useNavigate()
 
+    let prices = []
+
+    cartProducts?.map(product => {
+        prices.push(parseFloat(product.price))
+    })
+
+    const addTotal = (prices) => {
+        return prices.reduce(
+            (accumulator, number) => { 
+                return number + accumulator
+            },
+            0
+        )
+    }
+
+    const total = addTotal(prices)
+
+    console.log(total)
+
+
+
     return (
         <div>
-            <h3>the shopping cart component</h3>
+            <h2>the shopping cart component</h2>
             {cartProducts?.map(product => {
                 return <CartItem key={product.id} onRemoveClick={onRemoveClick} product={product}/>
             })}
+            <h3>Total: ${total}</h3>
         </div>
     )
 }
