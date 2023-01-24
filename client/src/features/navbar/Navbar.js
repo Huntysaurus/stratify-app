47a2baeaef76d40b694b5b388d7accf52c0f1c6a
Jacob Yaw <yawjacob@yahoo.com>
2023-01-23 21:32:35 -0600

    change menu navigation from select with option to ul div with boolean-state functionality

--- client/src/features/navbar/Navbar.js
+++ client/src/features/navbar/Navbar.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { logoutUser } from "../user/usersSlice";
 import styles from './navbar.module.css';
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false)
     const navigate = useNavigate()
     const dispatch = useDispatch()
+    
 
     function handleLogout() {
         dispatch(logoutUser())
@@ -23,28 +25,50 @@ function Navbar() {
         }
     }
 
-    return(
-        <>
-            {/* <div className={styles.navbar}>
-                <select className={styles.menu} onChange={(e)=>handleNavigation(e.target.value)}>
-                    <option value={""}>
-                        menu
-                    </option>
-                    <option value={"/shop"}>
-                        shop
-                    </option>
-                    <option value={"/profile"} >
-                        profile
-                    </option>
-                    <option value={"/logout"}>
-                        logout
-                    </option>
-                </select>
-            </div> */}
-            <nav className="navbar">
-                <ul className="navbar-nav"></ul>
-            </nav>
-        </>
+    return (
+            isOpen ? (
+                <>
+                    <button className={styles.menu_icon} onClick={()=> setIsOpen(!isOpen)}>menu</button>
+
+                    <div className={styles.navbar}>
+                    <ul>
+                        <li className={styles.list_option}
+                            value="/shop"
+                            onClick={(e)=>handleNavigation("/shop")}>
+                            shop
+                        </li>
+                        <li className={styles.list_option}
+                            value="/profile"
+                            onClick={(e)=>handleNavigation("/profile")}>
+                            profile
+                        </li>
+                        <li className={styles.list_option}
+                            value="/logout"
+                            onClick={(e)=>handleNavigation("/logout")}>
+                            logout
+                        </li>
+                    </ul>
+                </div>
+                </>
+            )
+            :
+            <button className={styles.menu_icon} onClick={()=> setIsOpen(!isOpen)}>menu</button>
+            // <div className={styles.navbar}>
+            //     <select className={styles.menu} onChange={(e)=>handleNavigation(e.target.value)}>
+            //         <option value={""}>
+            //             menu
+            //         </option>
+            //         <option value={"/shop"}>
+            //             shop
+            //         </option>
+            //         <option value={"/profile"} >
+            //             profile
+            //         </option>
+            //         <option value={"/logout"}>
+            //             logout
+            //         </option>
+            //     </select>
+            // </div>
     )
 }
 
